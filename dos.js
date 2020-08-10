125e1f0ce71706bebf5a458e283443cd4aefa369
VelazcoMicaela <49617711+VelazcoMicaela@users.noreply.github.com>
2020-08-10 10:05:02 -0300

    punto dos

--- dos.js
+++ dos.js
@@ -5,7 +5,8 @@ marca, precio, peso en kilogramos, tipo(sólido o líquido)
 a)informar el peso total de la compra.
 b)la marca del más caro de los líquidos
 c)la marca del más liviano de los sólidos
-*/function mostrar()
+*/
+function mostrar()
 {
 	var respuesta=true;
 	var marca;
